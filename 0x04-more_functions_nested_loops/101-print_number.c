9b02ad22521aa105b89197bbb8921fbed066dddd
Fashipe Paul <tpaul8593@gmail.com>
2022-09-15 14:27:18 -0700

    welcome

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,15 +5,18 @@
  * Return:void
  */
 void print number(int n)
+
 {
+
 unsigned int x:
 x = n;
-	if (n < 0)
-	{
-	putchar(45);
-	x = -n;
+if (n < 0)
+{
+putchar(45);
+x = -n;
 	}
-	if (x / 10)
-	print_number(x / 10);
-	_putchar ((x % 10) + '0');
+
+if (x / 10)
+print_number(x / 10);
+_putchar ((x % 10) + '0');
 }
